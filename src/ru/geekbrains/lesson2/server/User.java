9c6c2f17b0adbacf3ca286c3514f821d3ebd303c
Tukhel <Andrew_melf@mail.ru>
2019-05-13 16:11:40 +1000

    ДЗ2

---
+++ src/ru/geekbrains/lesson2/server/User.java
@@ -0,0 +1,48 @@
+package ru.geekbrains.lesson2.server;
+
+public class User {
+
+    private int id;
+    private String login;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + id + " "
+                + login + " " + password;
+    }
+}
